483b5676961c308090ab534090dab9b71590e64a
SmallMarben <mirrowjj@outlook.com>
2017-02-18 20:13:25 +0800

    Update config.php

--- oa/Common/Conf/config.php
+++ oa/Common/Conf/config.php
@@ -5,8 +5,8 @@ return array(
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
     'DB_NAME'               =>  'oa',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '1994',          // 密码
+    'DB_USER'               =>  'secret',      // 用户名
+    'DB_PWD'                =>  'secret',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'oa_',    // 数据库表前缀
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
